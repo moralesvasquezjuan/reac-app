d716ec239d5efe270507440436f9e98fc902979d
NoelHer <crisherper52@gmail.com>
2024-04-26 17:57:28 -0600

    Archivo validar registro

---
+++ src/pages/login/valiRegistr.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+export const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validateFormData(formData);
+    if (Object.keys(validationErrors).length === 0) {
+      // Realizar la acción de registro si no hay errores de validación
+      console.log("Formulario válido, enviar datos al servidor.");
+    } else {
+      // Mostrar errores de validación si los hay
+      setErrors(validationErrors);
+    }
+  };
+
+  const validateFormData = (data) => {
+    let errors = {};
+
+    if (!data.username.trim()) {
+      errors.username = "Username is required";
+    }
+
+    if (!data.email.trim()) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+      errors.email = "Email address is invalid";
+    }
+
+    if (!data.password.trim()) {
+      errors.password = "Password is required";
+    } else if (data.password.length < 6) {
+      errors.password = "Password must be at least 6 characters long";
+    }
+
+    if (data.password !== data.confirmPassword) {
+      errors.confirmPassword = "Passwords do not match";
+    }
+
+    return errors;
+  };
+};
